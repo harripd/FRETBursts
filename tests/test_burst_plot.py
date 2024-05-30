f373b04a9e111ab81b12e664882b9566ea07a57f
Paul Harris <harripd@gmail.com>
2024-05-30 20:47:55 +0300

    Require matplotlib tests

--- tests/test_burst_plot.py
+++ tests/test_burst_plot.py
@@ -13,14 +13,9 @@ import pytest
 import numpy as np
 
 
-try:
-    import matplotlib
-except ImportError:
-    has_matplotlib = False  # OK to run tests without matplotlib
-else:
-    has_matplotlib = True
-    matplotlib.use('Agg')  # but if matplotlib is installed, use Agg
-
+import matplotlib
+matplotlib.use('Agg')  # but if matplotlib is installed, use Agg
+import matplotlib.pyplot as plt
 # try:
 #     import numba
 # except ImportError:
@@ -37,8 +32,7 @@ from fretbursts import loader
 from fretbursts import select_bursts
 from fretbursts.ph_sel import Ph_sel
 from fretbursts.phtools import phrates
-if has_matplotlib:
-    import fretbursts.burst_plot as bplt
+import fretbursts.burst_plot as bplt
 
 
 # data subdir in the notebook folder
@@ -149,14 +143,17 @@ def data_alex(request):
 def test_mch_plot_bg(data_mch):
     d = data_mch
     bplt.mch_plot_bg(d)
+    plt.close()
 
 def test_mch_plot_bg_ratio(data_mch):
     d = data_mch
     bplt.mch_plot_bg_ratio(d)
+    plt.close()
 
 def test_mch_plot_bsize(data_mch):
     d = data_mch
     bplt.mch_plot_bsize(d)
+    plt.close()
 
 ##
 #  Timetrace plots
@@ -175,6 +172,7 @@ def test_trace_single(data, ratetraces):
     for i in range(d.nch):
         for ph_sel in ph_sel_list:
             bplt.dplot(d, ratetraces, i=i, ph_sel=ph_sel)
+    plt.close()
 
 @pytest.fixture(scope='module', params = (bplt.timetrace, bplt.ratetrace, 
                                           bplt.timetrace_bg, bplt.timetrace_fret,
@@ -186,7 +184,8 @@ def test_trace(data, timetraces):
     """Test general time trace type functions"""
     d = data
     for i in range(d.nch):
-            bplt.dplot(d, timetraces, i=i)
+        bplt.dplot(d, timetraces, i=i)
+    plt.close()
 
 
 @pytest.fixture(scope='module', params = (bplt.hist_size, bplt.hist_width,
@@ -204,12 +203,14 @@ def test_hist(data, hists):
     bplt.dplot(d, hists, i=None)
     for i in range(d.nch):
         bplt.dplot(d, hists, i=i)
+    plt.close()
 
 def test_hist_S(data_alex):
     d = data_alex
     bplt.dplot(d, bplt.hist_S, i=None)
     for i in range(d.nch):
         bplt.dplot(d, bplt.hist_S)
+    plt.close()
 
 @pytest.fixture(scope='module', params = (bplt.hist2d_alex, bplt.hexbin_alex,
                                           bplt.scatter_alex, bplt.scatter_naa_nt))
@@ -223,6 +224,7 @@ def test_ES_plots(data_alex, ES_plots):
         bplt.dplot(d, ES_plots, i=0, color_style='kde')
     for i in range(d.nch):
         bplt.dplot(d, ES_plots, i=i)
+    plt.close()
 
 @pytest.fixture(scope='module', params = (bplt.scatter_width_size, bplt.scatter_rate_da,
                                           bplt.scatter_fret_size, bplt.scatter_fret_nd_na,
@@ -235,3 +237,4 @@ def test_scatterplots(data, scatterplots):
     bplt.dplot(d, scatterplots, i=None)
     for i in range(d.nch):
         bplt.dplot(d, scatterplots, i=i)
+    plt.close()
